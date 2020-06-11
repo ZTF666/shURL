caa39dc732069b4c1a370ea50276f01a80d1e94b
ZTF <ZTF666@protonmail.ch>
2020-06-11 15:36:18 +0100

    :heart:

--- components/ToolBar.vue
+++ components/ToolBar.vue
@@ -30,9 +30,6 @@
         <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
     </v-app-bar>
-    <v-sheet id="scrolling-techniques-2" class="overflow-y-auto" max-height="600">
-      <v-container style="height: 1000px;"></v-container>
-    </v-sheet>
   </div>
 </template>
 
