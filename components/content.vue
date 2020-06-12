33415af5a06ab04e3ab3b1cdc377e673583ef68d
ZTF <ZTF666@protonmail.ch>
2020-06-12 08:37:48 +0100

    :heart:

--- components/content.vue
+++ components/content.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mt-10">
-    <v-form @submit.prevent="getLyrics()">
+    <v-form @submit.prevent="shortenURL()">
       <v-container>
         <v-layout row wrap class="d-flex justify-center">
           <v-flex xs8 sm6 md6>
@@ -9,7 +9,7 @@
                 <v-text-field label="Link" outline v-model="inLink" aria-autocomplete="false"></v-text-field>
               </v-col>
               <v-col cols="12" sm="6" md="6" class="mt-5">
-                <v-btn @click.prevent="getLyrics()">Generate Link</v-btn>
+                <v-btn @click.prevent="shortenURL()">Generate Link</v-btn>
               </v-col>
             </v-row>
             <v-row class="d-flex justify-center">
@@ -28,24 +28,24 @@ export default {
   data() {
     return {
       inLink: '',
-      api: 'https://api.shrtco.de/v2/'
+      shurl1: '',
+      shurl2: ''
     }
   },
-  methods:{
-    async shortenURL(){
-
-      try {
-        if (this.inLink != null) {
-          let response = await axios.get(
-            `https://api.shrtco.de/v2/` + this.inLink
-          )
-          console.log(response);
-          
+  methods: {
+    async shortenURL() {
+      if (this.inLink != null) {
+        let response = await axios.get(
+          `https://api.shrtco.de/v2/shorten?url=` + this.inLink
+        )
+        console.log(response.data.ok)
+        if (response.data.ok == 'true') {
+          this.shurl1 = response.data.result.short_link
+          this.shurl2 = response.data.result.short_link2
         }
-
-
+        // console.log(response.data.result.short_link)
+      }
     }
-
   }
 }
 </script>
