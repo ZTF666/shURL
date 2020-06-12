2274d5e42347158f2a77bdc2eaed3df61ca6f2b7
ZTF <ZTF666@protonmail.ch>
2020-06-12 08:41:02 +0100

    :heart:

--- components/content.vue
+++ components/content.vue
@@ -19,6 +19,29 @@
         </v-layout>
       </v-container>
     </v-form>
+    <!-- CONTENT -->
+
+    <v-card max-width="344" class="mx-auto">
+      <v-list-item>
+        <!-- <v-list-item-avatar color="grey"></v-list-item-avatar> -->
+        <v-list-item-content>
+          <v-list-item-title class="headline">Your Link is Ready !</v-list-item-title>
+          <!-- <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle> -->
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-img src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg" height="194"></v-img>
+
+      <v-card-text>Visit ten places on our planet that are undergoing the biggest changes today.</v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>mdi-floppy</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+    <!-- END CONTENT -->
   </div>
 </template>
 
