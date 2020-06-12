63ebee75a99a2edd6d523979e588af42f5cdf14d
ZTF <ZTF666@protonmail.ch>
2020-06-12 08:52:45 +0100

    :heart:

--- components/content.vue
+++ components/content.vue
@@ -4,16 +4,18 @@
       <v-container>
         <v-layout row wrap class="d-flex justify-center">
           <v-flex xs8 sm6 md6>
-            <v-row>
+            <v-row class="d-flex justify-center">
               <v-col cols="12" sm="6" md="6">
                 <v-text-field label="Link" outline v-model="inLink" aria-autocomplete="false"></v-text-field>
               </v-col>
-              <v-col cols="12" sm="6" md="6" class="mt-5">
-                <v-btn @click.prevent="shortenURL()">Generate Link</v-btn>
-              </v-col>
             </v-row>
+
             <v-row class="d-flex justify-center">
-              <div></div>
+              <div>
+                <v-col cols="12" sm="6" md="6" class="mt-5">
+                  <v-btn @click.prevent="shortenURL()">Generate Link</v-btn>
+                </v-col>
+              </div>
             </v-row>
           </v-flex>
         </v-layout>
@@ -21,18 +23,19 @@
     </v-form>
     <!-- CONTENT -->
 
-    <v-card max-width="344" class="mx-auto">
+    <v-card max-width="344" class="mx-auto mt-10">
       <v-list-item>
         <!-- <v-list-item-avatar color="grey"></v-list-item-avatar> -->
         <v-list-item-content>
-          <v-list-item-title class="headline">Your Link is Ready !</v-list-item-title>
+          <v-list-item-title class>{{msg}}</v-list-item-title>
           <!-- <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle> -->
         </v-list-item-content>
       </v-list-item>
 
-      <v-img src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg" height="194"></v-img>
+      <v-img src="https://picsum.photos/1920/1080?random" height="100"></v-img>
 
-      <v-card-text>Visit ten places on our planet that are undergoing the biggest changes today.</v-card-text>
+      <v-card-text>{{shurl1}}</v-card-text>
+      <v-card-text>{{shurl2}}</v-card-text>
 
       <v-card-actions>
         <v-spacer></v-spacer>
@@ -52,7 +55,8 @@ export default {
     return {
       inLink: '',
       shurl1: '',
-      shurl2: ''
+      shurl2: '',
+      msg: ''
     }
   },
   methods: {
@@ -62,9 +66,11 @@ export default {
           `https://api.shrtco.de/v2/shorten?url=` + this.inLink
         )
         console.log(response.data.ok)
-        if (response.data.ok == 'true') {
+        if (response.data.ok == true) {
           this.shurl1 = response.data.result.short_link
           this.shurl2 = response.data.result.short_link2
+          console.log(this.shurl1)
+          console.log(this.shurl2)
         }
         // console.log(response.data.result.short_link)
       }
